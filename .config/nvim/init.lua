75fd02f99d61877638fb533304a556c839d69a4d
Narayan Sreekumar <nsreekumar6@gmail.com>
2025-06-10 22:01:19 +0530

    GIT :/

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -68,6 +68,22 @@ end
 
 local opts = { noremap = true, silent = true }
 
+require'treesitter-context'.setup{
+  enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+  multiwindow = false, -- Enable multiwindow support.
+  max_lines = 4, -- How many lines the window should span. Values <= 0 mean no limit.
+  min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+  line_numbers = true,
+  multiline_threshold = 20, -- Maximum number of lines to show for a single context
+  trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+  mode = 'cursor',  -- Line used to calculate context. Choices: 'cursor', 'topline'
+  -- Separator between context and content. Should be a single character string, like '-'.
+  -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+  separator = nil,
+  zindex = 20, -- The Z-index of the context window
+  on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+}
+
 require 'lsp'
 require('blame').setup()
 -- require('mini.surround').setup()
@@ -175,6 +191,8 @@ vim.keymap.set('v', '<leader>fv', ":FzfLua grep_visual<CR>", { desc = "Search vi
 vim.keymap.set('n', '<leader>fR', ":FzfLua live_grep_resume<CR>", { desc = "Resume last grep", noremap = true, silent = true })
 vim.keymap.set('n', '<leader>fb', ":FzfLua grep_curbuf<CR>", { desc = "Search in current buffer", noremap = true, silent = true })
 vim.keymap.set('n', '<leader>fl', ":FzfLua live_grep<CR>", { desc = "Live grep", noremap = true, silent = true })
+vim.keymap.set('n', '<leader>fk', ":FzfLua keymaps<CR>", { desc = "Show this Menu", noremap = true, silent = true })
+vim.keymap.set('n', '<leader>ff', ":FzfLua files<CR>", { desc = "Files in cwd", noremap = true, silent = true })
 vim.keymap.set('n', '<leader>fp', function()
   require("fzf-lua").live_grep({ cwd = get_git_root() })
 end, { desc = "Grep through project (CWD)", noremap = true, silent = true })
